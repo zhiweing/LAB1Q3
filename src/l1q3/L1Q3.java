07876bc943523b9e56dc7ebbff332c00bbb96bf2
ngzhi <ngzhi@LAPTOP-D1DU237V>
2023-11-11 08:31:57 +0800

    Lab test 1(Q3)

---
+++ src/l1q3/L1Q3.java
@@ -0,0 +1,18 @@
+
+package l1q3;
+public class L1Q3 {
+
+    public static void main(String[] args) {
+        System.out.println("* * * * * * * * *                     * * *                     *                       *");
+        System.out.println("*               *                *            *                ***                    *     *");           
+        System.out.println("*               *               *              *              *****                 *         *");            
+        System.out.println("*               *               *               *               *                  *            *");             
+        System.out.println("*               *               *               *               *                *                *");
+        System.out.println("*               *               *               *               *                  *             *");
+        System.out.println("*               *                *             *                *                    *         *");
+        System.out.println("*               *                  *         *                  *                      *     *");
+        System.out.println("* * * * * * * * *                      ***                      *                         *");
+       
+    }
+    
+}
